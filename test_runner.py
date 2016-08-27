cd779b25b28f3550dbc952c2aabd3005215db2ec
Alistair McKelvie <alistair.mckelvie@gmail.com>
2016-08-27 12:58:54 +1000

    feat: Allow running individual files

--- test_runner.py
+++ test_runner.py
@@ -8,13 +8,16 @@ import pytest
 
 SDK_PATH = '/usr/local/google_appengine'
 
-def main(functional, sdk_path=SDK_PATH):
+def main(functional, sdk_path=SDK_PATH, files=[]):
     sys.path.insert(0, sdk_path)
 
     import dev_appserver
     dev_appserver.fix_sys_path()
 
-    if functional:
+    if files:
+        for file_ in files:
+            pytest.main(['-x', file_])
+    elif functional:
         pytest.main(['-x', 'functional_tests'])
     else:
         pytest.main(['-x', 'tests'])
@@ -22,11 +25,12 @@ def main(functional, sdk_path=SDK_PATH):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Run blog app tests.')
-    parser.add_argument('--functional', action='store_true')
+    parser.add_argument('files', type=str, nargs='*')
+    parser.add_argument('--functional', action='store_true', help='Run functional tests.')
     parser.add_argument('--sdk_path')
     args = parser.parse_args()
 
     if args.sdk_path:
-        main(args.functional, args.sdk_path)
+        main(args.functional, args.sdk_path, files=args.files)
     else:
-        main(args.functional)
+        main(args.functional, files=args.files)
