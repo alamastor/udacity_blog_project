c50d05548c1b614b0c9fef2a580be30f10e670be
Alistair McKelvie <alistair.mckelvie@gmail.com>
2016-09-14 11:42:43 +1000

    chore: Add fail fast option to test_runner

--- test_runner.py
+++ test_runner.py
@@ -8,7 +8,7 @@ import pytest
 
 SDK_PATH = '/usr/local/google_appengine'
 
-def main(functional, sdk_path=SDK_PATH, files=[]):
+def main(functional, sdk_path=SDK_PATH, files=[], fail_fast=False):
     sys.path.insert(0, sdk_path)
 
     import dev_appserver
@@ -16,21 +16,38 @@ def main(functional, sdk_path=SDK_PATH, files=[]):
 
     if files:
         for file_ in files:
-            pytest.main([file_])
+            cmd = [file_]
     elif functional:
-        pytest.main(['functional_tests'])
+        cmd = ['functional_tests']
     else:
-        pytest.main(['tests'])
+        cmd = ['tests']
+
+    if fail_fast:
+        cmd.append('-x')
+    pytest.main(cmd)
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Run blog app tests.')
     parser.add_argument('files', type=str, nargs='*')
-    parser.add_argument('--functional', action='store_true', help='Run functional tests.')
+    parser.add_argument(
+        '--functional', action='store_true', help='Run functional tests.'
+    )
     parser.add_argument('--sdk_path')
+    parser.add_argument(
+        '-x',
+        '--fail_fast', 
+        help="exit after first test fail",
+        action='store_true'
+    )
     args = parser.parse_args()
 
     if args.sdk_path:
-        main(args.functional, args.sdk_path, files=args.files)
+        main(
+            args.functional,
+            args.sdk_path,
+            files=args.files,
+            fail_fast=args.fail_fast
+        )
     else:
-        main(args.functional, files=args.files)
+        main(args.functional, files=args.files, fail_fast=args.fail_fast)
